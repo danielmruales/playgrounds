bebe4258a4b4dad54b6ed0b35ab48b88977147c1
Daniel Ruales <danielmruales@gmail.com>
2019-10-12 23:56:48 -0600

    made some spelling corrections

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div>
         <h1>
           Hello Everyone! <br/> 
-          If you have somehow stublled upon this page, <br/>
+          If you have somehow stumbled upon this page, <br/>
           let me breif you on it's purpose.
         </h1>
         <div className='img'><img src='https://www.frontier-enterprise.com/wp-content/uploads/2019/07/dassault-delmia.jpg' alt='two men stand at a high tech desk'/></div>
