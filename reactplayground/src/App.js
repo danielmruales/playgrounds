cd9ded3ff7658899b7db7a661195de6e88da36ba
Daniel Ruales <danielmruales@gmail.com>
2019-10-13 01:29:36 -0600

    added some text

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -10,6 +10,7 @@ function App() {
           let me breif you on it's purpose.
         </h1>
         <div className='img'><img src='https://www.frontier-enterprise.com/wp-content/uploads/2019/07/dassault-delmia.jpg' alt='two men stand at a high tech desk'/></div>
+        <p>This is a page full of imagination, creativity, new ideas, and so much more.</p>
     </div>
   );
 }
