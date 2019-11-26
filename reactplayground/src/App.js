39c094242e4da7c6d8f753bdc260f6dedb8c71f4
Daniel Ruales <danielmruales@gmail.com>
2019-11-26 01:45:35 -0700

    added foundation for a navbar

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './Home/Home.js'
+import Nav from './Nav/Nav.js'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav/>
       <Home/>
     </div>
   );
---
+++ reactplayground/src/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <div>
+            <li>
+                <p>Other Idea</p>
+                <p>Chatbot</p>
+                <p>Some Other Idea</p>
+            </li>
+        </div>
+    );
+};
+
+export default Nav;
