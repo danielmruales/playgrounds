b9ccd856e1c489428c29268c755663e15f615867
Daniel Ruales <danielmruales@gmail.com>
2019-11-26 01:25:11 -0700

    restructed the appby adding a nav and home component

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import Home from './Home/Home.js'
 import './App.css';
 
 function App() {
   return (
     <div>
-        <h1>
-          Hello Everyone! <br/> 
-          If you have somehow stumbled upon this page, <br/>
-          let me breif you on it's purpose.
-        </h1>
-        <div className='img'><img src='https://www.frontier-enterprise.com/wp-content/uploads/2019/07/dassault-delmia.jpg' alt='two men stand at a high tech desk'/></div>
-        <p>This is a page full of imagination, creativity, new ideas, and so much more.</p>
+      <Home/>
     </div>
   );
 }
---
+++ reactplayground/src/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './home.css'
+
+const Home = () => {
+    return (
+        <div>    
+            <h1> Hello Everyone! <br/> 
+                If you have somehow stumbled 
+                upon this page, <br/>
+                let me breif you on it's purpose.
+            </h1>
+        <div className='img'><img src='https://www.frontier-enterprise.com/wp-content/uploads/2019/07/dassault-delmia.jpg' alt='two men stand at a high tech desk'/></div>
+        <p>This is a page full of imagination, creativity, new ideas, and so much more.</p>   
+        </div>
+    );
+};
+
+export default Home;
